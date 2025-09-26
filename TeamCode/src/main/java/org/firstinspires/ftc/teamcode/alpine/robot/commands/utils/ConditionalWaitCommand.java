ca8a714ecb18a726b85d2f5e1f0fd9b0cb3fd6e3
PrathyushY <prathyushyeturi@gmail.com>
2025-09-26 19:03:23 -0400

    Added TelemetryUtil and new command classes, updated dependencies and build configurations.
    - Added TelemetryUtil for managing telemetry data with FtcDashboard.
    - Introduced command classes: ConditionalWaitCommand, DoCommand, and DoIfCommand for more flexible command scheduling.
    - Updated dependencies to include FtcDashboard, PedroPathing, and SolversLib.
    - Modified build.gradle files:
        - Updated compileSdkVersion to 34.
        - Enabled multiDex.
        - Added new Maven repositories.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alpine/robot/commands/utils/ConditionalWaitCommand.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.alpine.robot.commands.utils;
+
+import com.seattlesolvers.solverslib.command.CommandBase;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.function.BooleanSupplier;
+
+public class ConditionalWaitCommand extends CommandBase {
+    private final double seconds;
+    protected ElapsedTime timer;
+    protected BooleanSupplier condition;
+
+    /**
+     * Waits for given duration if the condition is true
+     *
+     * @param condition the condition whether to wait or not
+     * @param seconds the duration to wait (if condition is true)
+     */
+    public ConditionalWaitCommand(BooleanSupplier condition, double seconds) {
+        this.condition = condition;
+        this.seconds = seconds;
+    }
+
+    @Override
+    public void initialize() {
+        timer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
+        timer.reset();
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (condition.getAsBoolean()) {
+            return timer.seconds() >= this.seconds;
+        }
+
+        return true;
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alpine/robot/commands/utils/DoIfCommand.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.alpine.robot.commands.utils;
+
+import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.CommandBase;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.function.BooleanSupplier;
+
+public class DoIfCommand extends CommandBase {
+    private final double seconds;
+    protected ElapsedTime timer;
+    protected Command doOnTrue;
+    protected Command doOnFalse;
+    protected BooleanSupplier condition;
+    protected Command toExecute;
+
+    public DoIfCommand(Runnable doOnTrue, Runnable doOnFalse, BooleanSupplier condition, double seconds) {
+        this.doOnTrue = new InstantCommand(doOnTrue);
+        this.doOnFalse = new InstantCommand(doOnFalse);
+        this.condition = condition;
+        this.seconds = seconds;
+    }
+
+    public DoIfCommand(Command doOnTrue, Command doOnFalse, BooleanSupplier condition, double seconds) {
+        this.doOnTrue = doOnTrue;
+        this.doOnFalse = doOnFalse;
+        this.condition = condition;
+        this.seconds = seconds;
+    }
+
+    @Override
+    public void initialize() {
+        timer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
+        timer.reset();
+
+        // Decides what command should be run based on the state of the condition at the time of initialization
+        if (condition.getAsBoolean()) {
+            toExecute = doOnTrue;
+        } else {
+            toExecute = doOnFalse;
+        }
+
+        toExecute.initialize();
+    }
+
+    @Override
+    public void execute() {
+        toExecute.execute();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        toExecute.end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.seconds() >= this.seconds && toExecute.isFinished();
+    }
+}
