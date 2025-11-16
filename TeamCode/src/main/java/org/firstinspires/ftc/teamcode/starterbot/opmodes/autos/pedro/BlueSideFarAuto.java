e149b36272ceb677c6ff7a7d2b689208b513c93c
Hypothetical <adi.varshney09@gmail.com>
2025-11-15 23:34:03 -0500

    fixed naming again
    oopsie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterbot/opmodes/autos/pedro/BlueSideFarAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterbot/opmodes/autos/pedro/BlueSideFarAuto.java
@@ -7,7 +7,7 @@ import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "Blue Side Far Auto (Pedro, Mixed + Mechs)", group = "Pedro Autos")
+@Autonomous(name = "Blue Side Far Auto", group = "Pedro Autos")
 public class BlueSideFarAuto extends PedroBaseAuto {
 
     // poses
