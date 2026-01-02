48ce71014a3532c119d7be6cc45a87a098adea8d
PrathyushY <prathyushyeturi@gmail.com>
2026-01-02 18:39:22 -0500

    Update dependencies and add ShootingTester
    - Bump `pedropathing` from 2.0.4 to 2.0.5 in `build.dependencies.gradle`.
    - Add `slothboard:dashboard` dependency and fix syntax in `TeamCode/build.gradle`.
    - Increase launcher motor PIDF P coefficient from 300 to 350 in `Robot.java`.
    - Add `ShootingTester` teleop opmode for tuning ramp position and launcher velocity via FTC Dashboard.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterbot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterbot/Robot.java
@@ -105,7 +105,7 @@ public class Robot {
         blockerState = BlockerState.CLOSED;
         launchSequenceState = LaunchSequenceState.IDLE;
 
-        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, 0, 10));
+        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(350, 0, 0, 10));
 
         /*
          * Much like our drivetrain motors, we set the left feeder servo to reverse so that they
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterbot/opmodes/teleops/testers/ShootingTester.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.starterbot.opmodes.teleops.testers;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.starterbot.CommonTelemetry;
+import org.firstinspires.ftc.teamcode.starterbot.Constants;
+import org.firstinspires.ftc.teamcode.starterbot.Robot;
+
+@TeleOp(group = "testers")
+@Config
+public class ShootingTester extends LinearOpMode {
+    public static final double MAX_RAMP_DEGREES = 270.0;
+    public static final Pose goalPose = new Pose(12, 140);
+    public static double rampPosDegrees = 130.95; // 13.5 130.95
+    public static double launcherVelocityRadPerSec = 1.5;
+    public static boolean feedersOn = false;
+
+    @Override
+    public void runOpMode() {
+        CommonTelemetry.init(telemetry);
+        Robot.init(hardwareMap);
+        Robot.follower.setStartingPose(new Pose(56.5, 8.75, Math.toRadians(90)));
+        Robot.blocker.setPosition(Constants.BLOCKER_OPEN);
+
+        MultipleTelemetry mt = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            Robot.setFeederPower(feedersOn ? Constants.FEEDER_POWER : 0);
+
+            double rampPos = rampPosDegrees / MAX_RAMP_DEGREES;
+            Robot.ramp.setPosition(rampPos);
+
+            Robot.launcher.setVelocity(launcherVelocityRadPerSec, AngleUnit.RADIANS);
+
+            Robot.follower.update();
+            CommonTelemetry.draw(Robot.follower);
+
+            mt.addData("ramp pos", rampPos);
+            mt.addData("ramp pos deg", rampPosDegrees);
+            mt.addData("set launcher velocity (rad/s)", launcherVelocityRadPerSec);
+            mt.addData("read launcher velocity (rad/s)", Robot.launcher.getVelocity(AngleUnit.RADIANS));
+            mt.addData("distance to goal", getDistanceToGoal());
+            mt.update();
+        }
+    }
+
+    public double getDistanceToGoal() {
+        Pose currPose = Robot.follower.getPose();
+        return currPose.distanceFrom(goalPose);
+    }
+}
