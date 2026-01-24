ae26600757c9431acfb0dce6fa8839f3590a4e25
PrathyushY <prathyushyeturi@gmail.com>
2026-01-24 10:29:08 -0500

    fixed some values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterbot/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterbot/Constants.java
@@ -21,7 +21,7 @@ public class Constants {
     public static double INTAKE_POWER = 1; // Intake motors power
 
     public static double RAMP_INTAKE_POS = 0.0;
-    public static double RAMP_OUTTAKE_POS = 0.385;
+    public static double RAMP_OUTTAKE_POS = 0.4;
 
     public static double BLOCKER_OPEN = 0.1;
     public static double BLOCKER_CLOSED = 0.35;
