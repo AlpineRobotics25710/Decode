e4f0bf9666b3b32cfa43b2a1b3091d56bb7559f8
PrathyushY <prathyushyeturi@gmail.com>
2026-01-26 10:21:49 -0500

    limlight align changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterbot/opmodes/teleops/testers/LimelightAlignTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterbot/opmodes/teleops/testers/LimelightAlignTester.java
@@ -18,9 +18,12 @@ import java.util.List;
 @TeleOp(group = "testers")
 public class LimelightAlignTester extends LinearOpMode {
     public static int targetTagId = 20; //blue goal: 20, red goal: 24
+    public static double headingTolerance = 1.0;
+    public static double turnGain = 0.03;
+    public static boolean currentlyAligning = false;
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
         CommonTelemetry.init(telemetry);
         Robot.init(hardwareMap);
 
@@ -33,20 +36,42 @@ public class LimelightAlignTester extends LinearOpMode {
         Robot.follower.startTeleopDrive();
 
         while (!isStopRequested() && opModeIsActive()) {
-            Robot.follower.setTeleOpDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x * 1.1, gamepad1.right_stick_x);
-
             limelight.updateRobotOrientation(Math.toDegrees(Robot.follower.getHeading()));
 
             LLResult llResult = limelight.getLatestResult();
+            LLResultTypes.FiducialResult targetTag = null;
             if (llResult != null && llResult.isValid()) {
                 List<LLResultTypes.FiducialResult> detectedTags = llResult.getFiducialResults();
                 for (LLResultTypes.FiducialResult tag : detectedTags) {
                     if (tag.getFiducialId() == targetTagId) {
-                        CommonTelemetry.addData("Target x degrees", tag.getTargetXDegrees());
-                        CommonTelemetry.addData("Target y degrees", tag.getTargetYDegrees());
-                        CommonTelemetry.addData("target pos robot space", tag.getTargetPoseRobotSpace());
+                        targetTag = tag;
                     }
                 }
+
+                if (targetTag != null) {
+                    CommonTelemetry.addData("Target x degrees", targetTag.getTargetXDegrees());
+                    CommonTelemetry.addData("Target y degrees", targetTag.getTargetYDegrees());
+                    CommonTelemetry.addData("target pos robot space", targetTag.getTargetPoseRobotSpace());
+
+                    double headingError = targetTag.getTargetXDegrees();
+                    double turn = headingError * turnGain;
+                    CommonTelemetry.addData("heading error", headingError);
+                    CommonTelemetry.addData("turn power", turn);
+
+                    if (gamepad1.aWasPressed() && !currentlyAligning) {
+                        currentlyAligning = true;
+                    }
+
+                    if (currentlyAligning) {
+                        if (Math.abs(headingError) <= headingTolerance) currentlyAligning = false;
+
+                        Robot.mecanumDrive(0, 0, turn);
+                    } else {
+                        Robot.follower.setTeleOpDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x * 1.1, gamepad1.right_stick_x);
+                    }
+                }
+            } else {
+                CommonTelemetry.addData("Limelight", "No targets found");
             }
 
             Robot.follower.update();
